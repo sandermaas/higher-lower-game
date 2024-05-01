fd98f60fba79758f1201b8e51bdb67935da7aa2c
Sander Maas <sandermaas7@hotmail.com>
2024-05-01 17:19:30 +0200

    Vite basic project setup

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('vite').UserConfig} */
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()]
+});
